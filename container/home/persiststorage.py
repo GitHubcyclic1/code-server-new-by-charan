787ed26eee45268cd448fd7e8592877ee38af744
charan200415 <66833883+charan200415@users.noreply.github.com>
2022-03-30 22:43:33 +0530

    Update persiststorage.py

--- container/home/persiststorage.py
+++ container/home/persiststorage.py
@@ -2,12 +2,9 @@ import time
 import os
 data=os.getenv("RCLONE_DATA")
 app_name=os.getenv("APP_NAME")
-os.system(' rclone config file')
 #Config File
-os.system(f'echo {data} | base64 -d > /.config/rclone/rclone.conf')
-os.system(f'echo {data} | base64 -d > /.config/rclone/rclone.conf')
-os.system(f'echo {data} | base64 -d > /.config/rclone/rclone.conf')
-os.system(f'echo {data} | base64 -d > /.config/rclone/rclone.conf')
+os.system(f'mkdir -p /.config/rclone/')
+os.system(f'touch /.config/rclone/rclone.conf')
 os.system(f'echo {data} | base64 -d > /.config/rclone/rclone.conf')
 print('config File Created')
 #Download
