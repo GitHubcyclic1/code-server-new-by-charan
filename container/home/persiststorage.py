946571664181c0a67e5c1f9b10775a4d364a9b07
charan200415 <66833883+charan200415@users.noreply.github.com>
2022-03-31 07:13:54 +0530

    Update persiststorage.py

--- container/home/persiststorage.py
+++ container/home/persiststorage.py
@@ -1,17 +1,26 @@
 import time
 import os
+from datetime import datetime
+import pytz
+def print(log):
+    tz_india = pytz.timezone('Asia/Kolkata')
+    datetime_india = datetime.now(tz_india)
+    india=datetime_india.strftime("%H:%M:%S")
+    with open('logger','a') as logger:
+        logger.write(log+'==>'+india+'\n')
 data=os.getenv("RCLONE_DATA")
 app_name=os.getenv("APP_NAME")
+remote=os.getenv("RCLONE_REMOTE")
 #Config File
 os.system(f'mkdir -p /.config/rclone/')
 os.system(f'touch /.config/rclone/rclone.conf')
 os.system(f'echo {data} | base64 -d > /.config/rclone/rclone.conf')
 print('config File Created')
 #Download
-os.system(f'rclone sync test:{app_name} /app/WORKSPACE')
+os.system(f'rclone sync {remote}:{app_name} /app/WORKSPACE')
 print('files Synced')
 #upload Every 1 min
 while True:
-    os.system(f'rclone sync  /app/WORKSPACE test:{app_name}')
+    os.system(f'rclone sync  /app/WORKSPACE {remote}:{app_name}')
     print('Files Uploaded')
     time.sleep(60)
